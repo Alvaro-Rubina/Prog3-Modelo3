1355e552c17a849171099ef998da56e04513a16c
Alvaro Rubina <alvarorubina132@gmail.com>
2024-09-05 14:07:08 -0300

    Creada entidad Pedido

---
+++ src/main/java/org/example/model/entities/Pedido.java
@@ -0,0 +1,27 @@
+package org.example.model.entities;
+
+import lombok.*;
+import org.example.model.enums.Estado;
+import org.example.model.enums.TipoEnvio;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+@Builder
+public class Pedido {
+
+    //
+    private Long id;
+    private LocalDate fechaPedido;
+    private LocalTime horaEstimadaFinalizacion;
+    private double total;
+    private double totalCosto;
+    private Estado estado;
+    private TipoEnvio tipoEnvio;
+    private Set<DetallePedido> detalles = new HashSet<>();
+}
