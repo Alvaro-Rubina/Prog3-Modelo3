58ee50912996c85bea16b7b45d35380820b1a106
Alvaro Rubina <alvarorubina132@gmail.com>
2024-09-05 16:42:19 -0300

    Creadas instancias de UnidadMedida e Imagen
    Se crearon y persistieron en los respectivos repositorios instancias de UnidadMedida e Imagen.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -44,6 +44,64 @@ public class Main {
         clienteRepo.save(cliente3);
 
         // Creación de las unidades de medida
+        UnidadMedida ochoPorciones = UnidadMedida.builder()
+                .denominacion("porciones")
+                .build();
+
+        UnidadMedida cuatroPorciones = UnidadMedida.builder()
+                .denominacion("porciones")
+                .build();
+
+        UnidadMedida unLitro = UnidadMedida.builder()
+                .denominacion("litros")
+                .build();
+
+        unidadMedidaRepo.save(ochoPorciones);
+        unidadMedidaRepo.save(cuatroPorciones);
+        unidadMedidaRepo.save(unLitro);
+
+        // Creacion de las imagenes
+        Imagen imgPizzaHawaianaGrande = Imagen.builder()
+                .nombre("Pizza Hawaiana grande")
+                .url("https://i.pinimg.com/564x/27/24/4f/27244fdcfca45fcb75a4cdda7e1996f8.jpg")
+                .build();
+        Imagen imgPizzaNapolitanaGrande = Imagen.builder()
+                .nombre("Pizza Napolitana grande")
+                .url("https://www.lanacion.com.ar/resizer/v2/pizza-NUKNWKWWRBAGXEECJWGLKMULVQ.jpg?auth=df70fb70d27e9a242dfe70068af898769f18e832bb543cb49d3faf002314bb82&width=420&height=280&quality=70&smart=true")
+                .build();
+        Imagen imgPizzaMuzarellaGrande = Imagen.builder()
+                .nombre("Pizza Muzzarella grande")
+                .url("https://saborargento.com.ar/wp-content/uploads/2023/09/Receta-de-Pizza-Muzzarella.jpg")
+                .build();
+        Imagen imgPizzaHawaianaChica = Imagen.builder()
+                .nombre("Pizza Hawaiana chica")
+                .url("https://i.pinimg.com/564x/27/24/4f/27244fdcfca45fcb75a4cdda7e1996f8.jpg").
+                build();
+        Imagen imgPizzaNapolitanaChica = Imagen.builder()
+                .nombre("Pizza Napolitana chica")
+                .url("https://www.lanacion.com.ar/resizer/v2/pizza-NUKNWKWWRBAGXEECJWGLKMULVQ.jpg?auth=df70fb70d27e9a242dfe70068af898769f18e832bb543cb49d3faf002314bb82&width=420&height=280&quality=70&smart=true")
+                .build();
+        Imagen imgPizzaMuzarellaChica = Imagen.builder()
+                .nombre("Pizza Muzzarella chica")
+                .url("https://saborargento.com.ar/wp-content/uploads/2023/09/Receta-de-Pizza-Muzzarella.jpg")
+                .build();
+        Imagen imgCervezaAndes = Imagen.builder()
+                .nombre("Cerveza Andes")
+                .url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGKJJppEGa6tSBZ96kkU73C8q0jtHSfilAZg&s")
+                .build();
+        Imagen imgCervezaQuilmes = Imagen.builder()
+                .nombre("Cerveza Quilmes")
+                .url("https://graffica.info/wp-content/uploads/2016/09/Joplin-Quilmes-03-1-1024x681.png")
+                .build();
+
+        imagenRepo.save(imgPizzaHawaianaGrande);
+        imagenRepo.save(imgPizzaHawaianaChica);
+        imagenRepo.save(imgPizzaNapolitanaGrande);
+        imagenRepo.save(imgPizzaNapolitanaChica);
+        imagenRepo.save(imgPizzaMuzarellaGrande);
+        imagenRepo.save(imgPizzaMuzarellaChica);
+        imagenRepo.save(imgCervezaAndes);
+        imagenRepo.save(imgCervezaQuilmes);
 
     }
 
