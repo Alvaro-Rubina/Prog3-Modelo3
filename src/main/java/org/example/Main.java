1b096d795fb5220da65b8555abf66125dbf1936c
Alvaro Rubina <alvarorubina132@gmail.com>
2024-09-05 16:15:29 -0300

    Creadas instancias de Cliente
    Se crearon 3 instancias de Cliente y se guardaron en el repositorio

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,50 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import org.example.model.entities.*;
+import org.example.repository.InMemoryRepository;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.time.LocalDate;
+
+public class Main {
+
+    // Repositorios
+    public static InMemoryRepository<Articulo> articuloRepo = new InMemoryRepository<>();
+    public static InMemoryRepository<Cliente> clienteRepo = new InMemoryRepository<>();
+    public static InMemoryRepository<DetallePedido> detallePedidoRepo = new InMemoryRepository<>();
+    public static InMemoryRepository<Imagen> imagenRepo = new InMemoryRepository<>();
+    public static InMemoryRepository<Pedido> pedidoRepo = new InMemoryRepository<>();
+    public static InMemoryRepository<UnidadMedida> unidadMedidaRepo = new InMemoryRepository<>();
+
+    public static void main(String[] args) {
+
+        // Creación de los Clientes
+        Cliente cliente1 = Cliente.builder()
+                .nombre("JH")
+                .apellido("De la Cruz")
+                .telefono("0123456789")
+                .email("jh@gmail.com")
+                .fechaNacimiento(LocalDate.of(2000, 4,21)).build();
+
+        Cliente cliente2 = Cliente.builder()
+                .nombre("John")
+                .apellido("Doe")
+                .telefono("1231231230")
+                .email("johnDoe@gmail.com")
+                .fechaNacimiento(LocalDate.of(2004, 1,8)).build();
+
+        Cliente cliente3 = Cliente.builder()
+                .nombre("Miguel")
+                .apellido("Merentiel")
+                .telefono("1616161616")
+                .email("laBestiaMerentieeeeeeeeeel@gmail.com")
+                .fechaNacimiento(LocalDate.of(1996, 2,24)).build();
+
+        clienteRepo.save(cliente1);
+        clienteRepo.save(cliente2);
+        clienteRepo.save(cliente3);
+
+        // Creación de las unidades de medida
+
     }
+
 }
