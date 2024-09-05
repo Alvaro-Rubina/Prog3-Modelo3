03370a1276a0aacf491d498b1478f49228298b6d
Alvaro Rubina <alvarorubina132@gmail.com>
2024-09-05 14:14:52 -0300

    Creada entidad DetallePedido

---
+++ src/main/java/org/example/model/entities/DetallePedido.java
@@ -0,0 +1,20 @@
+package org.example.model.entities;
+
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+@Builder
+public class DetallePedido {
+
+    //
+    private Long id;
+    private int cantidad;
+    private double subTotal;
+    private Set<Articulo> articulos = new HashSet<>();
+
+}
