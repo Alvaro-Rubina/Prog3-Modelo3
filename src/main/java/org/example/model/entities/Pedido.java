f4e91a4b7c68dd4799a50111dd452a31ea612d1f
Alvaro Rubina <alvarorubina132@gmail.com>
2024-09-05 22:13:49 -0300

    Modificacion en clase Pedido
    - Se añadió el campo formaPago
    - Se añadieron metodos para determinar los campos horaEstimadaFinalizacion, total y totalCosto
    - Se añadió un constructor especifico para asignar dichos campos

--- src/main/java/org/example/model/entities/Pedido.java
+++ src/main/java/org/example/model/entities/Pedido.java
@@ -2,6 +2,7 @@ package org.example.model.entities;
 
 import lombok.*;
 import org.example.model.enums.Estado;
+import org.example.model.enums.MetodoPago;
 import org.example.model.enums.TipoEnvio;
 
 import java.time.LocalDate;
@@ -24,5 +25,59 @@ public class Pedido {
     private double totalCosto;
     private Estado estado;
     private TipoEnvio tipoEnvio;
+    private MetodoPago formaPago;
     private Set<DetallePedido> detalles = new HashSet<>();
+
+    //
+    public Pedido(HashSet<DetallePedido> detalles, TipoEnvio tipoEnvio, MetodoPago formaPago){
+        this.detalles = detalles;
+        this.tipoEnvio = tipoEnvio;
+        this.formaPago = formaPago;
+        this.estado = Estado.EN_PREPARACION;
+        this.fechaPedido = LocalDate.now();
+        this.horaEstimadaFinalizacion = setHoraEstimadaFinalizacion();
+        this.total = setTotal();
+        this.totalCosto = setTotalCosto();
+    }
+
+    //
+    private double setTotal() {
+        double valorTotal = 0;
+        for (DetallePedido detallePedido: detalles){
+            valorTotal += detallePedido.getSubTotal();
+        }
+        return valorTotal;
+    }
+
+    private double setTotalCosto(){
+        double valorCostoTotal = 0;
+        for (DetallePedido detallePedido: detalles){
+            valorCostoTotal += detallePedido.getArticulo().getPrecioCompra() * detallePedido.getCantidad();
+        }
+        return valorCostoTotal;
+    }
+
+    /*La hora estimada de finalizacion se determina de la siguiente forma:
+        Tiempo de preparacion del artículo que más demore + 15 minutos en caso de que haya más
+        de 3 articulos con tiempo de preparacion mayor a 0*/
+    private LocalTime setHoraEstimadaFinalizacion() {
+        int tiempoMaximo = 0;
+        int cantidadProductosConTiempoMayorACero = 0;
+
+       for (DetallePedido detallePedido: detalles){
+           if (detallePedido.getArticulo().getTiempoEstimadoEnMinutos() > tiempoMaximo ){
+               tiempoMaximo = detallePedido.getArticulo().getTiempoEstimadoEnMinutos();
+           }
+
+           if (detallePedido.getArticulo().getTiempoEstimadoEnMinutos() > 0){
+               cantidadProductosConTiempoMayorACero++;
+           }
+       }
+
+       if (cantidadProductosConTiempoMayorACero > 3){
+           tiempoMaximo += 15;
+       }
+
+       return LocalTime.now().plusMinutes(tiempoMaximo);
+    }
 }
