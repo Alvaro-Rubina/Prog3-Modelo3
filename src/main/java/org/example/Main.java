961db752e76463d23a1c60d9448ca90d694cf5fb
Alvaro Rubina <alvarorubina132@gmail.com>
2024-09-05 22:59:31 -0300

    Actualizada clase Main
    Se añadieron líneas de código para:
    
    - Simular ventas de cliente1, cliente2 y cliente3.
    - Imprimir por consola los datos de dichas ventas.
    - Mostrar la cantidad de veces que cliente1 compró.
    - Mostrar los productos que vende el negocio.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,13 @@
 package org.example;
 
 import org.example.model.entities.*;
+import org.example.model.enums.MetodoPago;
+import org.example.model.enums.TipoEnvio;
 import org.example.repository.InMemoryRepository;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
 
@@ -23,21 +27,26 @@ public class Main {
                 .apellido("De la Cruz")
                 .telefono("0123456789")
                 .email("jh@gmail.com")
-                .fechaNacimiento(LocalDate.of(2000, 4,21)).build();
+                .fechaNacimiento(LocalDate.of(2000, 4,21))
+                .pedidos(new HashSet<>())
+                .build();
 
         Cliente cliente2 = Cliente.builder()
                 .nombre("John")
                 .apellido("Doe")
                 .telefono("1231231230")
                 .email("johnDoe@gmail.com")
-                .fechaNacimiento(LocalDate.of(2004, 1,8)).build();
+                .fechaNacimiento(LocalDate.of(2004, 1,8))
+                .pedidos(new HashSet<>())
+                .build();
 
         Cliente cliente3 = Cliente.builder()
                 .nombre("Miguel")
                 .apellido("Merentiel")
                 .telefono("1616161616")
                 .email("laBestiaMerentieeeeeeeeeel@gmail.com")
-                .fechaNacimiento(LocalDate.of(1996, 2,24)).build();
+                .fechaNacimiento(LocalDate.of(1996, 2,24))
+                .pedidos(new HashSet<>()).build();
 
         clienteRepo.save(cliente1);
         clienteRepo.save(cliente2);
@@ -114,6 +123,7 @@ public class Main {
                 .unidadMedida(ochoPorciones)
                 .imagen(imgPizzaHawaianaGrande)
                 .build();
+        articuloRepo.save(pizzaHawaianaGrande);
 
         Articulo pizzaHawaianaChica = Articulo.builder()
                 .denominacion("Pizza Hawaiana chica")
@@ -125,6 +135,7 @@ public class Main {
                 .unidadMedida(cuatroPorciones)
                 .imagen(imgPizzaHawaianaChica)
                 .build();
+        articuloRepo.save(pizzaHawaianaChica);
 
         Articulo pizzaNapolitanaGrande = Articulo.builder()
                 .denominacion("Pizza Napolitana grande")
@@ -136,6 +147,7 @@ public class Main {
                 .unidadMedida(ochoPorciones)
                 .imagen(imgPizzaNapolitanaGrande)
                 .build();
+        articuloRepo.save(pizzaNapolitanaGrande);
 
         Articulo pizzaNapolitanaChica = Articulo.builder()
                 .denominacion("Pizza Napolitana chica")
@@ -147,6 +159,7 @@ public class Main {
                 .unidadMedida(cuatroPorciones)
                 .imagen(imgPizzaNapolitanaChica)
                 .build();
+        articuloRepo.save(pizzaNapolitanaChica);
 
         Articulo pizzaMuzarellaGrande = Articulo.builder()
                 .denominacion("Pizza Muzzarella grande")
@@ -158,6 +171,7 @@ public class Main {
                 .unidadMedida(ochoPorciones)
                 .imagen(imgPizzaMuzarellaGrande)
                 .build();
+        articuloRepo.save(pizzaMuzarellaGrande);
 
         Articulo pizzaMuzarellaChica = Articulo.builder()
                 .denominacion("Pizza Muzzarella chica")
@@ -169,6 +183,7 @@ public class Main {
                 .unidadMedida(cuatroPorciones)
                 .imagen(imgPizzaMuzarellaChica)
                 .build();
+        articuloRepo.save(pizzaMuzarellaChica);
 
         Articulo cervezaAndes = Articulo.builder()
                 .denominacion("Cerveza Andes")
@@ -180,6 +195,7 @@ public class Main {
                 .unidadMedida(unLitro)
                 .imagen(imgCervezaAndes)
                 .build();
+        articuloRepo.save(cervezaAndes);
 
         Articulo cervezaQuilmes = Articulo.builder()
                 .denominacion("Cerveza Quilmes")
@@ -191,7 +207,95 @@ public class Main {
                 .unidadMedida(unLitro)
                 .imagen(imgCervezaQuilmes)
                 .build();
+        articuloRepo.save(cervezaQuilmes);
 
+        // - El cliente1, realiza un pedido de una Pizza grande y una cerveza Quilmes.
+        DetallePedido detallePedido1 = new DetallePedido(pizzaMuzarellaGrande, 1);
+        DetallePedido detallePedido2 = new DetallePedido(cervezaQuilmes, 1);
+
+        Pedido pedido1 = new Pedido(new HashSet<>(Set.of(detallePedido1, detallePedido2)), TipoEnvio.NORMAL, MetodoPago.TRANSFERENCIA);
+        pedidoRepo.save(pedido1);
+
+        cliente1.getPedidos().add(pedido1);
+        clienteRepo.genericUpdate(1L, cliente1);
+
+        // - Mostrar por consola los datos del pedido del cliente, las líneas del detalle de pedido
+        // y el total a pagar.
+        mostrarPedido(1L, 1L);
+
+        // - El cliente2 realiza la compra de la 3 cervezas Quilmes y una Pizza Hawaiana chica.
+        DetallePedido detallePedido3 = new DetallePedido(cervezaQuilmes, 3);
+        DetallePedido detallePedido4 = new DetallePedido(pizzaHawaianaChica, 1);
+
+        Pedido pedido2 = new Pedido(new HashSet<>(Set.of(detallePedido3, detallePedido4)), TipoEnvio.EXPRESS, MetodoPago.EFECTIVO);
+        pedidoRepo.save(pedido2);
+
+        cliente2.getPedidos().add(pedido2);
+        clienteRepo.genericUpdate(2L, cliente2);
+
+        // - Mostrar por consola el pedido y detalle para que el cliente2 sepa cuánto deberá pagar
+        mostrarPedido(2L, 2L);
+
+        // -El cliente3 compra 1 Pizza grande Hawaiana , 1 pizza chica Muzza , 1 grande Napolitana
+        // y 2 cervezas Andes.
+        DetallePedido detallePedido5 = new DetallePedido(pizzaHawaianaGrande, 1);
+        DetallePedido detallePedido6 = new DetallePedido(pizzaMuzarellaChica, 1);
+        DetallePedido detallePedido7 = new DetallePedido(pizzaNapolitanaGrande, 1);
+        DetallePedido detallePedido8 = new DetallePedido(cervezaAndes, 2);
+
+        Pedido pedido3 = new Pedido(new HashSet<>(Set.of(detallePedido5, detallePedido6, detallePedido7, detallePedido8)),
+                TipoEnvio.EXPRESS, MetodoPago.DEBITO);
+        pedidoRepo.save(pedido3);
+
+        cliente3.getPedidos().add(pedido3);
+        clienteRepo.genericUpdate(3L, cliente3);
+
+        // - Mostrar por consola el pedido del cliente 3 y su total par apagar.
+        mostrarPedido(3L, 3L);
+
+        // - El cliente 1 en otro día realiza un pedido de 1 muzza chica y una cerveza Andes.
+        DetallePedido detallePedido9 = new DetallePedido(cervezaAndes, 1);
+
+        Pedido pedido4 = new Pedido(new HashSet<>(Set.of(detallePedido6, detallePedido9)), TipoEnvio.EXPRESS,
+                MetodoPago.EFECTIVO);
+        pedidoRepo.save(pedido4);
+
+        cliente1.getPedidos().add(pedido4);
+        clienteRepo.genericUpdate(1L, cliente1);
+
+        // - Mostrar por consola el pedido realizado por el cliente1 y el total de la compra
+        mostrarPedido(1L, 4L);
+
+        // - Mostar por consola la cantidad de veces que el cliente 1 compró en el local.
+        System.out.println("\n[Cantidad de veces que " +
+                clienteRepo.findById(1L).get().getNombre() + " " + clienteRepo.findById(1L).get().getApellido() +
+                " compró en el local: " + clienteRepo.findById(1L).get().getPedidos().size() + "]");
+
+        // - Mostrar todos los artículos que comercializa el negocio
+        System.out.println("\n- - - - - -  - - - \nArticulos que comercializa el negocio:");
+        for (Articulo ar: articuloRepo.findAll()){
+            System.out.println("    - " + ar.getDenominacion());
+        }
+
+    }
+
+    public static void mostrarPedido(Long idCliente, Long idPedido){
+        System.out.println("\n- - - PEDIDO DEL CLIENTE: " +
+                clienteRepo.findById(idCliente).get().getNombre() + " " +
+                clienteRepo.findById(idCliente).get().getApellido() + " - - -");
+        System.out.println("* Fecha del pedido: " + pedidoRepo.findById(idPedido).get().getFechaPedido());
+        System.out.println("* Hora estimada de finalizacion del pedido: " +
+                pedidoRepo.findById(idPedido).get().getHoraEstimadaFinalizacion());
+        System.out.println("* Estado del pedido: " + pedidoRepo.findById(idPedido).get().getEstado());
+        System.out.println("* Tipo de envío: " + pedidoRepo.findById(idPedido).get().getTipoEnvio());
+        System.out.println("* Forma de pago: " + pedidoRepo.findById(idPedido).get().getFormaPago());
+
+        System.out.println("- - - - - - - - - - - - - - - - \n* Articulos:");
+        for (DetallePedido detallePedido: pedidoRepo.findById(idPedido).get().getDetalles()){
+            System.out.println("    - " + detallePedido.getArticulo().getDenominacion() + " (" +
+                    detallePedido.getCantidad() + "), $" + detallePedido.getSubTotal());
+        }
+        System.out.println("- - - - - - - \n* TOTAL: " + pedidoRepo.findById(idPedido).get().getTotal());
     }
 
 }
